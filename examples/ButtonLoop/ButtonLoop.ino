99b797ea23e2c3a1ce8acd31e2304f9354ceb152
Lennart Hennigs <mail@lennarthennigs.de>
2022-04-04 15:12:44 +0200

    added loop() example

---
+++ examples/ButtonLoop/ButtonLoop.ino
@@ -0,0 +1,53 @@
+/////////////////////////////////////////////////////////////////
+
+#include "Button2.h"
+
+/////////////////////////////////////////////////////////////////
+
+#define BUTTON_PIN  2
+
+/////////////////////////////////////////////////////////////////
+
+Button2 button;
+
+/////////////////////////////////////////////////////////////////
+
+void setup() {
+  Serial.begin(9600);
+  delay(50);
+  Serial.println("\n\nButton Loop Demo");
+
+  button.begin(BUTTON_PIN);
+}
+
+/////////////////////////////////////////////////////////////////
+
+void loop() {
+  button.loop();
+
+  
+  Serial.println(button.clickToString(button.wait()));
+
+/* 
+ *  or replace the above line with (keep the loop):
+  
+  if (button.wasPressed()) {
+      switch(button.read()) {
+          case single_click: 
+            Serial.println("single");
+            break;
+          case double_click: 
+            Serial.println("double");
+            break;
+          case triple_click: 
+            Serial.println("triple");
+            break;
+          case long_click: 
+            Serial.println("looong");
+            break;
+      }
+  }
+*/
+  
+}
+/////////////////////////////////////////////////////////////////
