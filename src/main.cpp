081f84f979b6e7fa24246a56af150ea4eaeecf6f
Lennart Hennigs <mail@lennarthennigs.de>
2025-01-26 16:55:45 +0100

    added dummy file to be able to compile lib in VSCode

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+
+// this is needed for platformio to compile the library
+// it serves no other purpose
+
+#include "../examples/SingleButtonSimple/SingleButtonSimple.ino"
+#pragma message "compiling SingleButtonSimple.ino"
